28bae540194be44ee1c47fbabb2478b0792d6c78
Gayoung <gayoung.lee@webtoonscorp.com>
2017-11-27 01:27:54 +0000

    delete prebuilt

--- caffe/build/CMakeFiles/symlink_to_build.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/symlink_to_build"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/symlink_to_build.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
